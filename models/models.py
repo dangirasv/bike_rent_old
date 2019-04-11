15b864b0d7e30e3812dde88277bbd6b7114fc623
dangirasv <dangiras.venckus@gmail.com>
2019-04-11 16:58:40 +0300

    finished with basic bike rent module -  models, views and security files

--- models/models.py
+++ models/models.py
@@ -2,14 +2,23 @@
 
 from odoo import models, fields, api
 
-# class bike_rent(models.Model):
-#     _name = 'bike_rent.bike_rent'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class bike_rent(models.Model):
+    _name = 'bike.rent'
+    _description = 'basic class to hold bike rent info'
+
+    bike_name = fields.Char("Bike Model Name")
+    bike_description = fields.Text("Bike Description")
+    customer_name = fields.Char("Customer Name")
+    price = fields.Float("Bike Rent Price")
+    rent_start = fields.Date("Start of Rent Date", default=fields.Date.today)
+    rent_stop = fields.Date("End of Rent Date", default=fields.Date.today)
+    notes = fields.Text("Rent Notes")
+    image = fields.Binary("Bike Picture")
+
+
+"""
+Q:  classical view syntax vs new one
+    list vs tree
+    specific field for prices? 
+"""
