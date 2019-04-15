7d6eaa2bd5de6d73a6f7a27a4ddc50a5ee226619
dangirasv <dangiras.venckus@gmail.com>
2019-04-15 15:09:09 +0300

    removed unnecessary records, made changes according to Andrius notes

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': "bike_rent",
-
     'summary': "Bike rent management module.",
-
-    'description': """
-        A practice module meant for Versada internship that manages bike rent operations.
-    """,
-
+    'description': "A practice module meant for Versada internship that manages bike rent operations.",
     'author': "Dangiras",
     'website': "https://www.versada.eu",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '12.0.1.0',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
--- models/models.py
+++ models/models.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
-class bike_rent(models.Model):
+class BikeRent(models.Model):
     _name = 'bike.rent'
     _description = 'basic class to hold bike rent info'
 
@@ -12,15 +12,7 @@ class bike_rent(models.Model):
     customer_name = fields.Char("Customer Name")
     price = fields.Float("Bike Rent Price")
     rent_start = fields.Datetime("Rent Start Time", default=fields.Datetime.now)
-    rent_stop = fields.Datetime("End of Rent Time", default=fields.Datetime.now)
+    rent_stop = fields.Datetime("End of Rent Time")
     notes = fields.Text("Rent Notes")
     image = fields.Binary("Bike Picture")
-    name = fields.Char("Model name, mainly used for UI purposes", default='Rent Info')
-
-
-"""
-Q:  classical view syntax vs new one
-    list vs tree
-    specific field for prices?
-    individual configuration files
-"""
+    name = fields.Char("Model name, mainly used for UI purposes", default="Rent Info")
