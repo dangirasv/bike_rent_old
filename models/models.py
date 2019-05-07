7dd36f038b89cbead3a1930a9f94f8f84db3d54f
dangirasv <dangiras.venckus@gmail.com>
2019-05-07 18:28:22 +0300

    solved bugs with demo data not loading propertly and fixed product rent_ids

--- models/models.py
+++ models/models.py
@@ -41,6 +41,11 @@ class ProductTemplate(models.Model):
     is_bike = fields.Boolean(string="It's a Bike")
     manufacturer = fields.Char(string='Manufacturer')
     model = fields.Char(string='Model')
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
     rent_ids = fields.One2many('bike.rent', 'bike_id', string='Rent Records')
 
 
